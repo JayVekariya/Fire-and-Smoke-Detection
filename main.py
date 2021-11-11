c89f470f84a51e8201371c6286ab335df0c650d4
Vekariya Jay <67188055+JayVekariya@users.noreply.github.com>
2021-11-11 11:03:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ Fire_report=0;
 Alarm_status=False
 
 def alert():
-           playsound.playsound('')
+           playsound.playsound('alarm.wav')
 
-video=cv2.VideoCapture('fire2.jpg')
-lower = [18, 50, 50]
-upper = [35, 255, 255]
+video=cv2.VideoCapture('Smoke1.jpg')
+
+lower_fire = [18, 50, 50]
+upper_fire = [35, 255, 255]
+
 lower_smoke = [105, 1, 150]
 upper_smoke = [110, 50, 255]
 
@@ -27,15 +29,15 @@ while True:
 
     hsv=cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
 
-    lower=np.array(lower,dtype='uint8')
-    upper=np.array(upper,dtype='uint8')
+    lower_fire=np.array(lower_fire, dtype='uint8')
+    upper_fire=np.array(upper_fire, dtype='uint8')
 
     lower_smoke = np.array(lower_smoke, dtype='uint8')
     upper_smoke = np.array(upper_smoke, dtype='uint8')
 
     smoke_mask = cv2.inRange(hsv, lower_smoke, upper_smoke)
 
-    mask=cv2.inRange(hsv,lower,upper)
+    fire_mask=cv2.inRange(hsv,lower_fire,upper_fire)
 
     cv2.imshow('frame', frame)
     cv2.waitKey(0)
@@ -43,25 +45,28 @@ while True:
     cv2.imshow('hsv',blur)
     cv2.waitKey(0)
 
-    cv2.imshow('output',mask)
+    cv2.imshow('output',fire_mask)
     cv2.waitKey(0)
 
-    size = cv2.countNonZero(mask)
+    size_fire = cv2.countNonZero(fire_mask)
 
     size_smoke=cv2.countNonZero(smoke_mask)
 
-    print(size_smoke,size)
+    print(size_smoke,size_fire)
 
-    if size >= 10000 or size_smoke >=5000:
+    if size_fire >= 10000 or size_smoke >=5000:
              Fire_report=1
 
-    if Fire_report>=1:
+    if Fire_report == 1:
         if Alarm_status==False :
             alert()
             Alarm_status=True
 
             if cv2.waitKey(1) == 13:
                 break
+
+def alert():
+    playsound.playsound('')
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
